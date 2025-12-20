3abb444398202aafe37289c8f6bfe861c0292d74
juneba <junebajuneba@gmail.com>
2025-12-19 22:20:14 -0300

    create project conta-transf

---
+++ conta-transf/src/main/java/com/junior/conta_transf/ContaTransfApplication.java
@@ -0,0 +1,13 @@
+package com.junior.conta_transf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaTransfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaTransfApplication.class, args);
+	}
+
+}
---
+++ conta-transf/src/test/java/com/junior/conta_transf/ContaTransfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.conta_transf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaTransfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
