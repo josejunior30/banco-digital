c9d8c0e07d5f1b6e0810e58674f19b72b0d8f568
josejunior30 <josejuniordev@yahoo.com>
2026-04-09 20:58:42 -0300

    ajuste swagger

--- cliente/src/main/java/com/junior/cliente/DTO/ClienteRequestDTO.java
+++ cliente/src/main/java/com/junior/cliente/DTO/ClienteRequestDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,13 +13,12 @@ import jakarta.validation.constraints.Pattern;
 
 public record ClienteRequestDTO(
 
-		@NotBlank(message = "Nome é obrigatório") String name,
+		@Schema(example = "José Luiz Ribeiro Junior", description = "Nome completo do cliente") @NotBlank(message = "Nome é obrigatório") String name,
+		@Schema(example = "123.456.789-00", description = "CPF do cliente") @NotBlank(message = "CPF é obrigatório") @Pattern(regexp = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})", message = "CPF (11 dígitos) ou CNPJ (14 dígitos), com ou sem pontuação") String cpf,
 
-		@NotBlank(message = "CPF é obrigatório") @Pattern(regexp = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})", message = "CPF (11 dígitos) ou CNPJ (14 dígitos), com ou sem pontuação") String cpf,
+		@NotBlank(message = "Email é obrigatório") @Email(message = "Email inválido") String email, @Schema(
 
-		@NotBlank(message = "Email é obrigatório") @Email(message = "Email inválido") String email,
-
-		@NotNull(message = "Data de nascimento é obrigatória") @Past(message = "Data de nascimento deve ser no passado") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate birthDate
+				example = "25/12/1995", pattern = "^\\d{2}/\\d{2}/\\d{4}$") @NotNull(message = "Data de nascimento é obrigatória") @Past(message = "Data de nascimento deve ser no passado") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate birthDate
 
 ) {
 }
