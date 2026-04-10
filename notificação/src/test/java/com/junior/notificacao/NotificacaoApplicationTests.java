09274708d3224b25a9150bbd0a1b9dfc5f2ac2ba
josejunior30 <josejuniordev@yahoo.com>
2026-04-09 21:20:14 -0300

    criando projeto

---
+++ notificação/src/test/java/com/junior/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
